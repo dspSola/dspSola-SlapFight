c52276129b0add9baa82b02fb45977c349d762fe
DspSola <robin.sola@gmail.com>
2020-12-12 02:10:37 +0100

    TMP

--- Assets/Scripts/QTESys.cs
+++ Assets/Scripts/QTESys.cs
@@ -1,18 +1,137 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QTESys : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject _displayBox;
+    [SerializeField] GameObject _passBox;
+    [SerializeField] int _QTEGen;
+    [SerializeField] int _waitingForKey;
+    [SerializeField] int _correctKey;
+    [SerializeField] int _countingDown;
+
+    private void Update()
     {
-        
+        if (_waitingForKey == 0)
+        {
+            _QTEGen = Random.Range(1, 4);
+            _countingDown = 1;
+            StartCoroutine(CountDown());
+
+            if(_QTEGen == 1)
+            {
+                _waitingForKey = 1;
+                _displayBox.GetComponent<Text>().text = "[E]";
+            }
+            if (_QTEGen == 2)
+            {
+                _waitingForKey = 1;
+                _displayBox.GetComponent<Text>().text = "[R]";
+            }
+            if (_QTEGen == 3)
+            {
+                _waitingForKey = 1;
+                _displayBox.GetComponent<Text>().text = "[T]";
+            }
+        }
+
+        if(_QTEGen ==1)
+        {
+            if(Input.anyKeyDown)
+            {
+                if(Input.GetButtonDown("EKey"))
+                {
+                    _correctKey = 1;
+                    StartCoroutine(KeyPressing());
+                }
+                else
+                {
+                    _correctKey = 2;
+                    StartCoroutine(KeyPressing());
+                }
+            }
+        }
+
+        if (_QTEGen == 2)
+        {
+            if (Input.anyKeyDown)
+            {
+                if (Input.GetButtonDown("RKey"))
+                {
+                    _correctKey = 1;
+                    StartCoroutine(KeyPressing());
+                }
+                else
+                {
+                    _correctKey = 2;
+                    StartCoroutine(KeyPressing());
+                }
+            }
+        }
+        if (_QTEGen == 3)
+        {
+            if (Input.anyKeyDown)
+            {
+                if (Input.GetButtonDown("TKey"))
+                {
+                    _correctKey = 1;
+                    StartCoroutine(KeyPressing());
+                }
+                else
+                {
+                    _correctKey = 2;
+                    StartCoroutine(KeyPressing());
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator KeyPressing()
     {
-        
+        _QTEGen = 4;
+        if(_correctKey == 1)
+        {
+            _countingDown = 2;
+            _passBox.GetComponent<Text>().text = "PASS!";
+            yield return new WaitForSeconds(1.5f);
+            _correctKey = 0;
+            _passBox.GetComponent<Text>().text = "";
+            _displayBox.GetComponent<Text>().text = "";
+            yield return new WaitForSeconds(1.5f);
+            _waitingForKey = 0;
+            _countingDown = 1;
+        }
+        if (_correctKey == 2)
+        {
+            _countingDown = 2;
+            _passBox.GetComponent<Text>().text = "FAIL!";
+            yield return new WaitForSeconds(1.5f);
+            _correctKey = 0;
+            _passBox.GetComponent<Text>().text = "";
+            _displayBox.GetComponent<Text>().text = "";
+            yield return new WaitForSeconds(1.5f);
+            _waitingForKey = 0;
+            _countingDown = 1;
+        }
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(3.5f);
+        if(_countingDown == 1)
+        {
+            _QTEGen = 4;
+            _countingDown = 2;
+            _passBox.GetComponent<Text>().text = "FAIL!";
+            yield return new WaitForSeconds(1.5f);
+            _correctKey = 0;
+            _passBox.GetComponent<Text>().text = "";
+            _displayBox.GetComponent<Text>().text = "";
+            yield return new WaitForSeconds(1.5f);
+            _waitingForKey = 0;
+            _countingDown = 1;
+        }
     }
 }
